e0b790e05f97b60233b1c2981235bffaa27e09d4
liufengZY <liufeng23@lenovo.com>
2018-01-20 22:30:29 +0800

    修改搜索界面功能

--- pages/searchpage/searchpage.js
+++ pages/searchpage/searchpage.js
@@ -14,23 +14,25 @@ Page({
     //description, id, name, price, thumbnail.
     productitem: [],
 
-    searchLoading: false, //"上拉加载"的变量，默认false，隐藏  
-    searchLoadingComplete: false,//“没有数据”的变量，默认false，隐藏
+    footer_text:'',
+    showFooterText:false,
 
     noproduct: false, //没有找到商品，noproduct=true
 
     page: 1, //请求页面。
-
-    canReachButtom:false,
+    canShowMore:false,
 
     inputShowed: false,
-    thisKeyWord: "",
+    inputVal:'',
+
+    keyword:'',
+
   },
 
   showInput: function () {
     console.log("shwoInput");
     this.setData({
-      thisKeyWord: "",
+      inputVal: "",
       inputShowed: true
     });
   },
@@ -38,21 +40,27 @@ Page({
   //搜索商品
   searchItems: function () {
     console.log("searchItems");
-    //查询网络
+    //查询服务器search接口
     this.doRequestSearch();
-
+    this.setData({
+      inputVal: "",
+      inputShowed: true
+    });
   },
 
   clearInput: function () {
     console.log("clearInput");
     this.setData({
-      thisKeyWord: ""
+      inputVal: "",
+      keyword:'',
+      inputShowed: true
     });
   },
 
   inputTyping: function (e) {
     console.log("inputTyping", e);
-    this.setData({ thisKeyWord: e.detail.value });
+    this.setData({ inputVal: e.detail.value, keyword: e.detail.value});
+    this.setData({ noproduct: false});
   },
 
   /**
@@ -103,8 +111,14 @@ Page({
   onPullDownRefresh: function () {
     console.log("onPullDownRefresh");
     this.setData({ page: 1 });
-    this.setData({ searchLoading: false });
-    this.setData({ searchLoadingComplete: false });
+    this.setData({ canShowMore: false });
+    this.setData({ showFooterText: false });
+    this.setData({ footer_text: '' });
+    this.setData({
+      productitem: [],
+      noproduct: false
+    });
+    this.doRequestSearch();
   },
 
   /**
@@ -112,27 +126,23 @@ Page({
    */
   onReachBottom: function () {
     console.log("onReachBottom");
-    if (this.data.canReachButtom) {
-      this.setData({ searchLoading: true });
-      this.setData({ searchLoadingComplete: false });
+    if (this.data.canShowMore) {
       //触发搜索，分页请求。
       this.data.page++;
       //请求服务器更多的数据
       this.doRequestSearch();
     }
-    // showtoast.showSuccess('搜索请求成功完成');
   },
 
   doRequestSearch: function () {
     //请求网络接口，search接口。
     var that = this;
-    var keyword = that.data.thisKeyWord;
-    console.log("wxSearchFn,keyword:", keyword);
+    console.log("Search,keyword:", that.data.keyword);
     // api.request() 方法和 wx.request() 方法使用是一致的，不过如果用户已经登录的情况下，会把用户的会话信息带给服务器，服务器可以跟踪用户
     api.request({
       // 要请求的地址
       url: config.server.requestSearchItem,
-      data: { session: Session.Session.get(), keyword: keyword, page: that.data.page },
+      data: { session: Session.Session.get(), keyword: that.data.keyword, page: that.data.page },
       // 请求之前是否登陆，如果该项指定为 true，会在请求之前进行登录
       header: {
         'content-type': 'application/json' // 默认值
@@ -140,12 +150,7 @@ Page({
       method: 'GET',
       success(result) {
         console.log("search, result:", result.data);
-        if (result.data.list.lenght >= 20) {
-          that.setData({canReachButtom: true});
-        } else {
-           that.setData({canReachButtom: false});
-        }
-        if (result.data.list.length != 0) {
+        if (result.data.list.length === 20 ) {
           for (var i = 0; i < result.data.list.length; i++) {
             //拼接humbnail的完整路径名字。
             result.data.list[i].thumbnail = config.imgUrlPrefix + result.data.list[i].thumbnail;
@@ -154,16 +159,36 @@ Page({
             productitem: result.data.list,
             noproduct: false
           });
-          that.setData({ searchLoading: false });
-          that.setData({ searchLoadingComplete: true });
-        } else {
+          that.setData({ canShowMore: true });
+          that.setData({ showFooterText: true });
+          that.setData({ footer_text: '上拉加载更多商品' });
+        } else if (result.data.list.length > 0 && result.data.list.length < 20) {
+          for (var i = 0; i < result.data.list.length; i++) {
+            //拼接humbnail的完整路径名字。
+            result.data.list[i].thumbnail = config.imgUrlPrefix + result.data.list[i].thumbnail;
+          }
           that.setData({
-            productitem: [],
-            noproduct: true
+            productitem: result.data.list,
+            noproduct: false
           });
-          that.setData({ searchLoading: false });
-          that.setData({ searchLoadingComplete: false });
-        }
+          that.setData({ canShowMore: false});
+          that.setData({ showFooterText: true });
+          that.setData({ footer_text: '已经搜索加载了全部商品' });
+        } else if (result.data.list.length === 0) {
+          if (that.data.canShowMore) {
+            that.setData({ showFooterText: true });
+            that.setData({ footer_text: '已经搜索加载了全部商品' });
+            return;
+          } else {
+            that.setData({
+              productitem: [],
+              noproduct: true
+            });
+            that.setData({ canShowMore: false });
+            that.setData({ showFooterText: false });
+            that.setData({ footer_text: '' });
+          }
+        } 
       },
 
       fail(error) {
